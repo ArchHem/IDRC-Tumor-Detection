33ca10b068dd1cbef1d39209102b7361f6984818
ArchHem <szabo.bende321@gmail.com>
2022-03-07 15:12:29 +0000

    Explicitly binary classification, using class weights.

--- first_attempt.py
+++ first_attempt.py
@@ -8,9 +8,10 @@ from tensorflow import keras
 from keras import layers
 from keras.models import Sequential
 
+import pathlib
 """DELETE THE images subdirectory to run, otherwise it will recognize a non-valid directory!"""
 
-import pathlib
+
 """Link to pre-split dataset"""
 imgs = pathlib.Path('Images1')
 
@@ -24,6 +25,7 @@ together with img1.show()"""
 positives = list(imgs.glob('pos/*'))
 
 negatives = list(imgs.glob('non/*'))
+
 
 """We need to think about this, we need to look into how it resizes
 images, might introduce noise - Bendeguz"""
@@ -82,41 +84,56 @@ def hsv_conversion(x):
     return tf.image.rgb_to_hsv(x)
 
 """transform to rgb first or nah? layers.Lambda(hsv_conversion, input_shape = (height, width, 3))"""
+"""Exclude random rotation (?)"""
 
 dimensions = 3
+kernel_s = 3
+neg, pos = len(negatives), len(positives)
+
+
+"""single neuron output - binary model"""
 model = Sequential([
   layers.Resizing(height,width),
   layers.Rescaling(1./255, input_shape=(height, width, dimensions)),
-  layers.Conv2D(16, dimensions, padding='same', activation='relu'),
+  layers.RandomZoom(0.1),
+  layers.Conv2D(16, kernel_s, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Conv2D(32, dimensions, padding='same', activation='relu'),
+  layers.Conv2D(32, kernel_s, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Conv2D(64, dimensions, padding='same', activation='relu'),
+  layers.Conv2D(64, kernel_s, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+  layers.Dense(1, activation = 'sigmoid')
 ])
+"""class weights"""
+cl_wght = {0: (1 / neg) * ((neg + pos) / 2.0), 1:(1 / pos) * ((neg + pos) / 2.0)}
+print(cl_wght)
+"""From_logits = True crashes it, pls investigate why
 
+tried, did not work, returning to weighted example: tfa.losses.SigmoidFocalCrossEntropy(from_logits=False, alpha = 0.25, gamma = 2)"""
+"""try adding precision label: https://github.com/huggingface/transformers/issues/10075"""
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 
 
-epochs=10
+epochs = 15
+
 history = model.fit(
   train_ds,
   validation_data=val_ds,
-  epochs=epochs
+  epochs=epochs,
+  class_weight = cl_wght
 )
-
+print(history.history)
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
 loss = history.history['loss']
 val_loss = history.history['val_loss']
-
 epochs_range = range(epochs)
 
 plt.figure(figsize=(8, 8))
@@ -132,7 +149,8 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 
+model.save('models/model4')
 
-model.save('models/model1')
+model.summary()
 
 plt.show()

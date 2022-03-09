6becad13c18c35f586c1dfbc629dce13b86b43fd
ArchHem <84734676+ArchHem@users.noreply.github.com>
2022-03-09 16:24:52 +0000

    Add files via upload

---
+++ color_init.py
@@ -0,0 +1,12 @@
+import shutil
+
+N_white = 9500
+N_black = 500
+
+"""
+for i in range(N_white):
+    shutil.copy2('color_tests/white/white000.png', 'color_tests/white/white{:04d}.png'.format(i+1))
+
+for j in range(N_black):
+    shutil.copy2('color_tests/black/black000.png', 'color_tests/black/black{:04d}.png'.format(j + 1))
+"""
